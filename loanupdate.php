bc03b968e54577c53b7c0a0c652215642845274f
shubhampal123 <31174909+shubhampal123@users.noreply.github.com>
2019-03-23 16:30:35 +0530

    Add files via upload
    Update Loan of Customer-Take input.

---
+++ loanupdate.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+        <title></title>
+UPDATE LOAN 
+</head>
+<body>
+<br>
+<br>
+       <form
+        action="loanupdate1.php" method="post">
+        Id                      :<input type="text" name="id"><br><br>
+        Amount                  :<input type="number" name="amount"><br><br>
+        <button>SUBMIT</button>
+       </form>
+</body>
+</html>
+
+
+
