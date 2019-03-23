eb9cf8ab123781289a00c950886b9a9a2b6da61b
shubhampal123 <31174909+shubhampal123@users.noreply.github.com>
2019-03-23 16:04:44 +0530

    Add files via upload
    Add new Customer-Take input.

---
+++ insertcustomer.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+ADD DATA
+</head>
+<body>
+<br>
+<br>
+       <form
+       	action="insertcustomer1.php" method="post">
+       	Id                      :<input type="text" name="id"><br><br>
+        Password                :<input type=:"text" name="password"><br><br>
+       	Name                    :<input type="text" name="name"><br><br>
+       	City                    :<input type="text" name="city"><br><br>
+       	Street                  :<input type="text" name="street"><br><br>
+       	<button>SUBMIT</button>
+       </form>
+</body>
+</html>
