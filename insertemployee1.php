92b67b52a98ed6fa5ca05f1e2170594c3ece3287
shubhampal123 <31174909+shubhampal123@users.noreply.github.com>
2019-03-23 16:17:33 +0530

    Add files via upload
    Add new Employee-Insert into database.

---
+++ insertemployee1.php
@@ -0,0 +1,37 @@
+<?php 
+  $conn =new mysqli("localhost","root","root","bank") ;
+  if($conn->connect_error)
+  {
+        die("fuck of".$conn->connect_error) ;
+  }
+  
+   $var1 = mysqli_real_escape_string($conn,$_POST['name']);
+   $var5=mysqli_real_escape_string($conn,$_POST['password']) ;
+$var2 = mysqli_real_escape_string($conn,$_POST['id']);
+$var3 = mysqli_real_escape_string($conn,$_POST['street']);
+$var4 = mysqli_real_escape_string($conn,$_POST['date']);
+  $sql ="INSERT INTO EMPLOYEE  VALUES('".$var2."','".$var1."','".$var3."','".$var4."')" ;
+  $sql1="INSERT INTO EMPLOYEEPASSWORD  VALUES('".$var2."','".$var5."')" ;
+
+  if($conn->query($sql)==TRUE)
+  {
+        echo "Success" ;
+
+  }
+  else
+  {
+        echo "fuck" ;
+  }
+   if($conn->query($sql1)==TRUE)
+  {
+        echo "Success" ;
+
+  }
+  else
+  {
+        echo "fuck".$sql."<br>".$conn->error ;
+  }
+ 
+  $conn->close() ;
+ ?>
+
