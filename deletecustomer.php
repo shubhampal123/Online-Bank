c2b302c498a5dd2ce6cff64dc7f4c31fdacaf8af
shubhampal123 <31174909+shubhampal123@users.noreply.github.com>
2019-03-23 16:12:35 +0530

    Add files via upload
    Delete Customer-Take input.

---
+++ deletecustomer.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <title></title>
+DELETE DATA
+</head>
+<body>
+<br>
+<br>
+       <form
+        action="deletecustomer1.php" method="post">
+        Id   :<input type="text" name="id"><br><br>
+        <button>SUBMIT</button>
+       </form>
+</body>
+</html>
