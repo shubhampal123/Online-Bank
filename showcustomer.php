e001fff8bb5d6da97d49bded1f1a907869e1f510
shubhampal123 <31174909+shubhampal123@users.noreply.github.com>
2019-03-23 16:15:45 +0530

    Add files via upload
    Show all customer from database.

---
+++ showcustomer.php
@@ -0,0 +1,24 @@
+
+<?php 
+  $conn =new mysqli("localhost","root","root","bank") ;
+  if($conn->connect_error)
+  {
+  	die("fuck of".$conn->connect_error) ;
+  }
+  $sql ="SELECT CUSTOMER_ID,CUSTOMER_NAME  FROM CUSTOMER " ;
+  $result=$conn->query($sql) ;
+  if($result->num_rows>0)
+  {
+  	
+  	while($row=$result->fetch_assoc())
+  	{
+  		echo "id:    ".$row["CUSTOMER_ID"]."   - name:    ".$row["CUSTOMER_NAME"]."<br><br>" ;
+  	}
+
+  }
+  else
+  {
+  	echo "0 results" ;
+  }
+  $conn->close() ;
+ ?>
