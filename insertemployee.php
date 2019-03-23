86b4d9e191f5911e2551accf4a2bd47cf16432bf
shubhampal123 <31174909+shubhampal123@users.noreply.github.com>
2019-03-23 16:16:52 +0530

    Add files via upload
    Add new Employee-Take input.

---
+++ insertemployee.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+        <title></title>
+ADD DATA
+</head>
+<body>
+<br>
+<br>
+       <form
+        action="insertemployee1.php" method="post">
+        Id                      :<input type="text" name="id"><br><br>
+        Password                :<input type=:"text" name="password"><br><br>
+        Name                    :<input type="text" name="name"><br><br>
+        Date                    :<input type="date" name="date"><br><br>
+        Street                  :<input type="text" name="street"><br><br>
+        <button>SUBMIT</button>
+       </form>
+</body>
+</html>
+
+
