b86db02e5d02b4a0078ebca18f841d2aaeccb02b
shubhampal123 <31174909+shubhampal123@users.noreply.github.com>
2019-03-23 16:23:31 +0530

    Add files via upload
    Delete Customer-Delete from database.

---
+++ deletecustomer1.php
@@ -0,0 +1,33 @@
+<?php 
+  $conn =new mysqli("localhost","root","root","bank") ;
+  if($conn->connect_error)
+  {
+        die("fuck of".$conn->connect_error) ;
+  }
+  
+  
+$var = mysqli_real_escape_string($conn,$_POST['id']);
+  $sql ="DELETE FROM CUSTOMER WHERE CUSTOMER_ID='".$var."' " ;
+  $sql1="DELETE FROM CUSTOMERPASSWORD WHERE id='".$var."' " ;
+  if($conn->query($sql)==TRUE)
+  {
+        echo "Success" ;
+
+  }
+  else
+  {
+        echo "fuck".$sql."<br>".$conn->error ;
+  }
+   if($conn->query($sql1)==TRUE)
+  {
+        echo "Success" ;
+
+  }
+  else
+  {
+        echo "fuck".$sql."<br>".$conn->error ;
+  }
+
+  $conn->close() ;
+ ?>
+
