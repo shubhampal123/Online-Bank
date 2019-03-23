7d1b6dafaa17dd3af647cad60c0b7a81d2929986
shubhampal123 <31174909+shubhampal123@users.noreply.github.com>
2019-03-23 16:01:35 +0530

    Add files via upload
    Interface containing functionalities of customer.

---
+++ customer.php
@@ -0,0 +1,41 @@
+
+<!DOCTYPE html>
+<head>
+</head>
+<body>
+<h1>WELCOME CUSTOMER!</h1>
+<br>
+<br>
+<br>
+<form
+   action="accountinfo.php" method="post">
+   <button>UPDATE INFO</button>
+   </form>
+<br>
+<br>
+<form action="loaninfo.php" method ="post">
+    <button>LOAN INFORMAtION</button>
+    </form>
+     <?php
+	session_start();
+   	 $name=$_GET['user'] ;
+	$_SESSION["NameGlobal"]=$name;
+?>
+
+  
+<?php
+
+       $connection=mysqli_connect("localhost","root","root","bank")or die("FUCK u".mysqli_connect_error()) ;
+       if($connection){
+            $name=$_GET['user'] ;
+            $sql="SELECT * FROM CUSTOMER WHERE CUSTOMER_ID='".$name."'" ;
+            $result=$connection->query($sql) ;
+            while($r=$result->fetch_assoc())
+            {
+               echo "id: ".$r["CUSTOMER_ID"]."|| name: ".$r["CUSTOMER_NAME"]."|| street: ".$r[CUSTOMER_STREET]."|| city: ".$r[CUSTOMER_CITY]."<br><br>" ;
+           }
+
+     mysql_close($connection) ;
+}
+?>
+
