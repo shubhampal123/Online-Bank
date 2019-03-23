0b98532f135e61f30e3b24aa33958a9c3298c7db
shubhampal123 <31174909+shubhampal123@users.noreply.github.com>
2019-03-23 16:24:35 +0530

    Add files via upload
    Show all employee information.

---
+++ showemployee.php
@@ -0,0 +1,26 @@
+<?php 
+  $conn =new mysqli("localhost","root","root","bank") ;
+  if($conn->connect_error)
+  {
+        die("fuck of".$conn->connect_error) ;
+  }
+  $sql ="SELECT EMPLOYEE_ID,EMPLOYEE_NAME  FROM EMPLOYEE " ;
+  $result=$conn->query($sql) ;
+  if($result->num_rows>0)
+  {
+
+        while($row=$result->fetch_assoc())
+        {
+                echo "id:    ".$row["EMPLOYEE_ID"]."   - name:    ".$row["EMPLOYEE_NAME"]."<br><br>" ;
+        }
+
+  }
+  else
+  {
+        echo "0 results" ;
+  }
+  $conn->close() ;
+ ?>
+
+
+
