1943e0e4af6af925c7c9d34f251162dc734a9932
shubhampal123 <31174909+shubhampal123@users.noreply.github.com>
2019-03-23 15:59:12 +0530

    Add files via upload
    Authenticate user id and password of employee.

---
+++ employeelogin.php
@@ -0,0 +1,34 @@
+<?php
+
+       $connection=mysqli_connect("localhost","root","root","bank")or die("FUCK u".mysqli_connect_error()) ;
+       if($connection){
+
+
+        if(isset($_POST["name"],$_POST["password"]))
+        {       
+
+                $name=$_POST["name"] ;
+                $password=$_POST["password"] ;
+
+$r=mysqli_query($connection,"SELECT id,pass FROM EMPLOYEEPASSWORD WHERE id='".$name."' AND pass ='".$password."'") ;
+          $c=mysqli_fetch_array($r) ;
+
+                if(isset($c))
+                {
+                    $user=$name ;
+                    header("Location:employee.php?user=".$user) ;
+                    exit() ;
+                }
+                else
+                {
+                        echo "Login Failed" ;
+                }
+        }
+           else
+                {
+                        echo "Information not entered " ;
+                }
+   mysqli_close($connection) ;
+ }
+ ?>
+
