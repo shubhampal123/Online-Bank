982243477d78cd4351ff2a3670aa6897b66631fc
shubhampal123 <31174909+shubhampal123@users.noreply.github.com>
2019-03-23 16:28:59 +0530

    Add files via upload
    Apply loan for customer-Take input.

---
+++ loanapply.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+        <title></title>
+ADD DATA
+</head>
+<body>
+<br>
+<br>
+       <form
+        action="loanapply1.php" method="post">
+        Customer Id             :<input type="text" name="id"><br><br>
+        Loan Id                 :<input type="number" name="loanid"><br><br>         
+        LOan Amount             :<input tyoe="number" name="amount"><br><br>
+        <button>SUBMIT</button>
+       </form>
+</body>
+</html>
+
+
