7ebb7f0c9704f298fef1c4557528c74a1c391daa
shubhampal123 <31174909+shubhampal123@users.noreply.github.com>
2019-04-22 17:15:30 +0530

    Update insertemployee1.php

--- insertemployee1.php
+++ insertemployee1.php
@@ -2,7 +2,7 @@
   $conn =new mysqli("localhost","root","root","bank") ;
   if($conn->connect_error)
   {
-        die("fuck of".$conn->connect_error) ;
+        die("Cannot connect to the database ".$conn->connect_error) ;
   }
   
    $var1 = mysqli_real_escape_string($conn,$_POST['name']);
@@ -20,7 +20,7 @@ $var4 = mysqli_real_escape_string($conn,$_POST['date']);
   }
   else
   {
-        echo "fuck" ;
+        echo "Error " ;
   }
    if($conn->query($sql1)==TRUE)
   {
@@ -29,7 +29,7 @@ $var4 = mysqli_real_escape_string($conn,$_POST['date']);
   }
   else
   {
-        echo "fuck".$sql."<br>".$conn->error ;
+        echo "Error ".$sql."<br>".$conn->error ;
   }
  
   $conn->close() ;
