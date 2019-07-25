d40786d40ea3ef56bf8a26f51b0d1927a50c5b63
shubhampal123 <31174909+shubhampal123@users.noreply.github.com>
2019-07-25 16:23:06 +0530

    Update insertcustomer.php

--- insertcustomer.php
+++ insertcustomer.php
@@ -9,11 +9,11 @@ ADD DATA
 <br>
        <form
        	action="insertcustomer1.php" method="post">
-       	Id                      :<input type="text" name="id"><br><br>
-        Password                :<input type=:"text" name="password"><br><br>
-       	Name                    :<input type="text" name="name"><br><br>
-       	City                    :<input type="text" name="city"><br><br>
-       	Street                  :<input type="text" name="street"><br><br>
+       	Id                      :<input type="text" name="id" pattern=".{6,}"><br><br>
+        Password                :<input type=:"text" name="password" pattern=".{6,}"><br><br>
+       	Name                    :<input type="text" name="name" pattern="[A-Za-z]{3,}"><br><br>
+       	City                    :<input type="text" name="city" pattern="[A-Za-z]{3,}"><br><br>
+       	Street                  :<input type="text" name="street" pattern="[A-Za-z]{3,}"><br><br>
        	<button>SUBMIT</button>
        </form>
 </body>
