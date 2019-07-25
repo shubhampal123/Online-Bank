9aecd6281f84b69351e6f79509147776b64f417c
shubhampal123 <31174909+shubhampal123@users.noreply.github.com>
2019-07-25 15:21:42 +0530

    Add files via upload

---
+++ transfermoney.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="projectstyle.css">
+	<title>
+		
+	</title>
+
+</head>
+<body>
+	<h2>TRANSFER MONEY</h2>
+  <br>
+  <br>
+     <form
+      action="transfermoney1.php" method="post">
+      <input type="text" name="id" required placeholder="Id of receiver"><br><br>
+      <input type="integer" name="amount" required placeholder="Amount"><br><br>
+      <button>Submit</button>
+     	
+     </form>
+
+
+</body>
+</html>
