c1a8edfe390b15de4d4e2b1c55d623cd487c7480
shubhampal123 <31174909+shubhampal123@users.noreply.github.com>
2019-07-25 15:24:06 +0530

    Add files via upload

---
+++ caccount.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+Add details
+</head>
+<body>
+<form
+  action="caccount1.php" method="post">
+ account  id        :<input type="text" name="name" ><br><br>
+ balance   :<input type="numeric" name="balance" ><br><br>
+ date     :<input type="date" name="date" ><br><br>
+ interest rate  :<input type="text" name="rate" ><br><br>
+  <button>Submit</button>
+</form>
+
+</body>
+</html>
+
+
