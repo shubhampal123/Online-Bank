8f7b201fdbd1b574d390991866b0fe8437adebe9
shubhampal123 <31174909+shubhampal123@users.noreply.github.com>
2019-07-26 20:45:31 +0530

    Update transfermoney1.php

--- transfermoney1.php
+++ transfermoney1.php
@@ -2,7 +2,7 @@
   $conn =new mysqli("localhost","root","root","bank") ;
   if($conn->connect_error)
   {
-        die("fuck of".$conn->connect_error) ;
+        die("Error ".$conn->connect_error) ;
   }
      $var1 = mysqli_real_escape_string($conn,$_POST['id']);
     $var2 = mysqli_real_escape_string($conn,$_POST['amount']);
